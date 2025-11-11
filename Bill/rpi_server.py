a4899f9811b439f1482bb2e6b606fca74c92a45d
BillSavart <energyy0512@gmail.com>
2025-11-12 01:25:38 +0800

    server done

--- Bill/rpi_server.py
+++ Bill/rpi_server.py
@@ -72,6 +72,7 @@ def handle_command(cmd: str) -> str:
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
     print(f"Server started. Listening on port {PORT}...")
